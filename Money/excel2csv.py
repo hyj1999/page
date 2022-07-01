d5d59220d98e64a835d7c9b3d8552cd78a47848f
weixin_43964993 <huangyijie1999@163.com>
2022-07-01 15:03:55 +0800

    update 记账本

---
+++ Money/excel2csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def main(path = ''):
+    data = pd.read_excel('money.xlsx', sheet_name=None, index_col=0)
+    for sheet in data.keys():
+        data[sheet].to_csv(path + '%s.csv' % sheet)
+    with open(path + "sheet_name.txt", 'w') as f:
+        f.write("sheet_name = ")
+        f.write("[")
+        f.write(", ".join(['"' + sheet_name + '"' for sheet_name in data.keys()]))
+        f.write("];")
+
+if __name__ == '__main__':
+    main()
