aa5fd1d6db606ba3de19a2fc9d6273273bbd1877
hyj <1343768408@qq.com>
2023-03-05 19:50:10 +0800

    update 记账本

--- Money/excel2csv.py
+++ Money/excel2csv.py
@@ -1,11 +1,40 @@
 # pip install pandas
 
 import pandas as pd
+import base64
 
 def main(path = ''):
     data = pd.read_excel('money.xlsx', sheet_name=None, index_col=0)
     for sheet in data.keys():
         data[sheet].to_csv(path + '%s.csv' % sheet, encoding='utf-8')
+    with open(path + "sheet_name.txt", 'w', encoding='utf-8') as f:
+        f.write("sheet_name = ")
+        f.write("[")
+        f.write(", ".join(['"' + sheet_name + '"' for sheet_name in data.keys()]))
+        f.write("];")
+
+
+def base64_encode_string(s):
+    """
+    将字符串进行Base64编码
+    """
+    s_bytes = s.encode('UTF-8')  # 将字符串转换为字节串
+    base64_bytes = base64.b64encode(s_bytes)  # 进行Base64编码
+    base64_string = base64_bytes.decode('UTF-8')  # 将字节串转换为字符串
+    return base64_string.strip()  # 去除换行符并返回编码后的字符串
+
+
+def main_base64(path = ''):
+    data = pd.read_excel('money.xlsx', sheet_name=None, index_col=0)
+    for sheet in data.keys():
+        data[sheet].to_csv(path + '%s.csv' % sheet, encoding='utf-8')
+        with open(path + '%s.csv' % sheet, encoding='utf-8') as file:
+            text = file.read()
+            text_base64 = base64_encode_string(text)
+            file.close()
+        with open(path + '%s.csv' % sheet, 'w', encoding='utf-8') as file:
+            file.write(text_base64)
+            file.close()
     with open(path + "sheet_name.txt", 'w', encoding='utf-8') as f:
         f.write("sheet_name = ")
         f.write("[")
